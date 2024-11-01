ea44a9b36603c9d3205875ed0ecd905e9813bb85
parham-devdocs <p.77.pazargadi@gmail.com>
2024-11-01 17:15:46 +0330

    subjects  page created

--- src/app/(dashboard)/subjects/page.tsx
+++ src/app/(dashboard)/subjects/page.tsx
@@ -3,35 +3,22 @@ import React from "react";
 import Image from "next/image";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
-import { role, parentsData } from "@/lib/data";
+import { role, subjectsData } from "@/lib/data";
 import Link from "next/link";
 
-interface Parent {
+interface Subject {
   id: number;
-  parentId: string;
-  name: string;
-  email?: string;
-  students: string[];
-  address: string;
-  phone?: string;
+    name: string;
+    teachers:string[]
+ 
 }
 const columns = [
-  { header: "Info", accessor: "info" },
+  { header: "Subject Names", accessor: "name" },
 
   {
-    header: "Phone",
-    accessor: "phone",
+    header: "Teachers",
+    accessor: "teachers",
     className: "hidden md:table-cell",
-  },
-  {
-    header: "Students",
-    accessor: "students",
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Address",
-    accessor: "address",
-    className: "hidden lg:table-cell",
   },
   { header: "Actions", accessor: "actions" },
 ];
@@ -39,28 +26,23 @@ const columns = [
 const Row = ({
   id,
 
-  students,
-  name,
-  email,
-  phone,
-
-  address,
-}: Parent) => {
+teachers,
+    name,
+  
+ 
+}: Subject) => {
   return (
     <tr key={id} className="even:bg-purpleLight hover:bg-purple">
       {" "}
       {/* Added key prop */}
       <td className="flex gap-4 items-center ml-3 ">
-        <div className="flex flex-col gap-2">
-          <h3 className="font-medium">{name}</h3>
-          <h4>{email && email}</h4>
-        </div>
+              <p>{ name }</p>
       </td>
-      <td className="hidden md:table-cell text-center">{students.join(",")}</td>
-      <td className="hidden md:table-cell text-center">{phone}</td>
-      <td className="hidden md:table-cell text-center">{address}</td>
+   
+      <td className="hidden md:table-cell text-center">{teachers.join(",")}</td>
+         
       <td className="flex gap-2 items-center mt-5 justify-center  ">
-        <Link href={`/parents/${id}`}>
+        <Link href={`/subjects/${id}`}>
           <button className="bg-sky rounded-full p-2">
             <Image src={"/view.png"} alt="View" width={15} height={15} />
           </button>
@@ -75,12 +57,12 @@ const Row = ({
   );
 };
 
-const ParentsPage = () => {
+const SubjectsPage = () => {
   return (
     <div className=" bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className=" flex justify-between items-center">
-        <h1 className=" font-semibold text-xl hidden md:block">All Parents</h1>
+        <h1 className=" font-semibold text-xl hidden md:block">All Subjects</h1>
         <div className=" flex flex-col  md:flex-row items-center gap-4  w-full md:w-auto">
           <TableSearch />
           <div className=" flex gap-2">
@@ -112,11 +94,11 @@ const ParentsPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} data={parentsData} renderRow={Row} />
+      <Table columns={columns} data={subjectsData} renderRow={Row} />
       {/* PAGINATION  */}
       <Pagination />
     </div>
   );
 };
 
-export default ParentsPage;
+export default SubjectsPage;
